c45c36adbdb66c719c369ef95b2b58b2a12bb3bd
Shawn <pan.conan@gmail.com>
2019-12-26 13:32:41 +0800

    add response time

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const express = require('express')
 const app = express()
 const {checkReqMethod} = require('./config/check-req-method')
-
+const responseTime = require('response-time')
 const port = 3000
 
+app.use(responseTime(function (req, res, time) {
+  // Q.請問有辦法把我寫好的/config/check-req-method.js直接導入拿來用嗎?
+  if (req.url !== '/favicon.ico') {
+    const url = req.url
+    const method = req.method
+    date = new Date
+    const yy = date.getFullYear()
+    const mm = date.getMonth() + 1
+    const dd = date.getDate()
+    const hours = date.getHours()
+    const mins = date.getMinutes()
+    const secs = date.getSeconds()
+    console.log(yy, "-", mm, "-", dd, " ", hours,":", mins, ":", secs, "|",method, "from", url, "|", Math.round(time), 'ms')
+  }
+}))
+
 // 列出全部 Todo
-app.get('/', checkReqMethod ,(req, res) => {
-    res.send('列出全部 Todos')
+app.get('/', (req, res) => {
+  // Q.為什麼checkReqMethod不能放在這呢?
+  res.send('列出全部 Todos')
 })
 
 // 新增一筆 Todo 頁面
-app.get('/new', checkReqMethod, (req, res, next) => {
+app.get('/new', (req, res) => {
     res.send('新增 Todo 頁面')
 })
  
 // 顯示一筆 Todo 的詳細內容
-app.get('/:id',checkReqMethod , (req, res) => {
+app.get('/:id', (req, res) => {
     res.send('顯示一筆 Todo')
 })
 
 // 新增一筆  Todo
-app.post('/',checkReqMethod , (req, res) => {
+app.post('/', (req, res) => {
     res.send('新增一筆  Todo')
 })
 
-app.delete('/:id/delete', checkReqMethod, (req, res) => {
+app.delete('/:id/delete', (req, res) => {
   res.send('刪除 Todo')
 })
 
--- config/check-req-method.js
+++ config/check-req-method.js
@@ -1,16 +1,20 @@
 module.exports = {
     checkReqMethod : function(req, res, next) {
-        const url = req.url
-        const method = req.method
-        
-        date = new Date
-        const yy = date.getFullYear()
-        const mm = date.getMonth() + 1
-        const dd = date.getDate()
-        const hours = date.getHours()
-        const mins = date.getMinutes()
-        const secs = date.getSeconds()
-        console.log(yy, "-", mm, "-", dd, " ", hours,":", mins, ":", secs, "|",method, "from", url)
-        next()
+        /* Q.請問這樣處理/favicon.ico的請求是對的嗎?
+        如果沒這樣做，每次都會在終端機跑出兩次的console.log
+        */
+        if (req.url !== '/favicon.ico') {
+            const url = req.url
+            const method = req.method
+            date = new Date
+            const yy = date.getFullYear()
+            const mm = date.getMonth() + 1
+            const dd = date.getDate()
+            const hours = date.getHours()
+            const mins = date.getMinutes()
+            const secs = date.getSeconds()
+            console.log(yy, "-", mm, "-", dd, " ", hours,":", mins, ":", secs, "|",method, "from", url)
+            next()
+        }
     }
 }
